84b1086e4bc46afd463fb9559461e266e3034db0
Alabi Temitope <t.banji@rocketmail.com>
2023-11-27 05:45:53 -0800

    updated

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,25 +4,25 @@ import GitHubButton from "react-github-btn";
 const Navbar = () => (
   <nav>
     <h2>
-      <a href="https://github.com/olashubomicode/screen-recorder-react">
+      <a href="https://github.com/code2adebayo/screen-recorder-react">
         use-screen-recorder
       </a>
       <GitHubButton
-        href="https://github.com/olashubomicode/screen-recorder-react"
+        href="https://github.com/code2adebayo/screen-recorder-react"
         data-show-count="true"
-        aria-label="Star olashubomicode/screen-recorder-react on GitHub"
+        aria-label="Star code2adebayo/screen-recorder-react on GitHub"
       >
         Star
       </GitHubButton>
       <GitHubButton
-        href="https://github.com/olashubomicode"
+        href="https://github.com/code2adebayo"
         data-show-count="true"
-        aria-label="Follow @olashubomicode on GitHub"
+        aria-label="Follow @code2adebayo on GitHub"
       >
-        Follow @olashubomicode
+        Follow @code2adebayo
       </GitHubButton>
     </h2>
-    <a href="https://azeez-coder.netlify.app">More in portfolio</a>
+    <a href="https://shina-code.netlify.app">More in portfolio</a>
   </nav>
 );
 
